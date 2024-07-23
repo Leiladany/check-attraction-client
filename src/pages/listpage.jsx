225a76cbf34c0fb9993749ded2aac55f20e7ab02
Leiladany <leiladany16@gmail.com>
2024-07-23 17:06:10 +0100

    add typography, button and input for Lists page

--- src/pages/listpage.jsx
+++ src/pages/listpage.jsx
@@ -1,11 +1,15 @@
-import { Stack } from "@mui/joy";
+import { Stack, Typography, Input, Button } from "@mui/joy";
 
 export const ListPage = () => {
 
   return (
     <Stack sx={{ gap: { xs: 1, lg: 3 } }}>
-      <Stack sx={{ alignItems: "center", mt: 8 }}>
-
+      <Stack sx={{ gridTemplateColumns: { xs: "1fr 1fr", lg: "1fr 1fr 1fr" }, flexDirection:"row" }}>
+      <Typography>
+        #id Lists
+      </Typography>
+      <Button sx={{width:"180px", borderRadius:"20px",}}>Create List</Button>
+      <Input sx={{width:"180px", mr:"20px", borderRadius:"20px", border:"2px solid #8A00F7"}} placeholder="Search List" />
       </Stack>
     </Stack>
   );
